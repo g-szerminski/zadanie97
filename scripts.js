86c1e925ca60ae3034595bc1128532d8617faec0
Grzegorz Szermiński <szerman@poczta.fm>
2018-02-12 12:19:04 +0100

    Add js content

--- scripts.js
+++ scripts.js
@@ -49,10 +49,58 @@ function newGame() {
 	gameState = 'started';
 	setGameElements();
 	playerNameElem.innerHTML = player.name;
-    // setGamePoints(); // This function has not been created yet
+	//setGamePoints(); // This function has not been created yet
 	}
 }
 
 function playerPick(playerPick) {
     console.log(playerPick);
+}
+
+function getComputerPick() {
+	var possiblePicks = ['rock', 'paper', 'scissors'];
+	return possiblePicks[Math.floor(Math.random()*3)];
+}
+
+var playerPickElem = document.getElementById('js-playerPick'),
+	computerPickElem = document.getElementById('js-computerPick'),
+	playerResultElem = document.getElementById('js-playerResult'),
+	computerResultElem = document.getElementById('js-computerResult');
+
+function playerPick(playerPick) {
+	var computerPick = getComputerPick();
+	playerPickElem.innerHTML = playerPick;
+	computerPickElem.innerHTML = computerPick;
+}
+
+function checkRoundWinner(playerPick, computerPick) {
+	playerResultElem.innerHTML = computerResultElem.innerHTML = '';
+	var winnerIs = 'player';
+	if (playerPick == computerPick) {
+		winnerIs = 'noone'; // remis
+	} else if (
+		(computerPick == 'rock' &&  playerPick == 'scissors') ||
+		(computerPick == 'scissors' &&  playerPick == 'paper') ||
+		(computerPick == 'paper' &&  playerPick == 'rock')) {
+		winnerIs = 'computer';
+	}
+	if (winnerIs == 'player') {
+		playerResultElem.innerHTML = "Win!";
+		player.score++;
+	} else if (winnerIs == 'computer') {
+		computerResultElem.innerHTML = "Win!";
+		computer.score++;
+	}
+}
+
+function playerPick(playerPick) {
+	var computerPick = getComputerPick();
+	playerPickElem.innerHTML = playerPick;
+	computerPickElem.innerHTML = computerPick;
+	checkRoundWinner(playerPick, computerPick);
+}
+
+function setGamePoints() {
+	playerPointsElem.innerHTML = player.score;
+	computerPointsElem.innerHTML = computer.score;
 }
